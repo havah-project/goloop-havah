9d73a1f3dc1c5aad65de5e4071b0aa9ec6e224f2
MoonKyu Song <mksong@iconloop.com>
2022-06-13 11:48:33 +0900

    Remove override handlers for ICON

--- havah/contractmanager.go
+++ havah/contractmanager.go
@@ -17,13 +17,10 @@
 package havah
 
 import (
-	"github.com/icon-project/goloop/common/errors"
 	"github.com/icon-project/goloop/service/state"
 
 	"math/big"
 
-	"github.com/icon-project/goloop/common"
-	"github.com/icon-project/goloop/common/codec"
 	"github.com/icon-project/goloop/common/db"
 	"github.com/icon-project/goloop/common/log"
 	"github.com/icon-project/goloop/module"
@@ -33,8 +30,6 @@ import (
 type contractManager struct {
 	contract.ContractManager
 	log log.Logger
-
-	eeTypes state.EETypes
 }
 
 func (cm *contractManager) GetSystemScore(contentID string, cc contract.CallContext, from module.Address, value *big.Int) (contract.SystemScore, error) {
@@ -44,73 +39,18 @@ func (cm *contractManager) GetSystemScore(contentID string, cc contract.CallCont
 	return cm.ContractManager.GetSystemScore(contentID, cc, from, value)
 }
 
+var javaOnly = state.EETypeFilter{
+	state.JavaEE: true,
+}
+
 func (cm *contractManager) DefaultEnabledEETypes() state.EETypes {
-	return cm.eeTypes
+	return javaOnly
 }
-
-func (cm *contractManager) GenesisTo() module.Address {
-	return state.ZeroAddress
-}
-
-var govAddress = common.MustNewAddressFromString("cx0000000000000000000000000000000000000001")
-
-func (cm *contractManager) GetHandler(from, to module.Address, value *big.Int, ctype int, data []byte) (contract.ContractHandler, error) {
-	if (ctype == contract.CTypeTransfer || ctype == contract.CTypeCall) && !to.IsContract() {
-		return newTransferHandler(from, to, value, false, cm.log), nil
-	}
-	ch, err := cm.ContractManager.GetHandler(from, to, value, ctype, data)
-	if err != nil {
-		return nil, err
-	}
-	if (ctype == contract.CTypeDeploy || ctype == contract.CTypeCall) &&
-		to.Equal(govAddress) {
-		return newGovernanceHandler(ch), nil
-	}
-	if (ctype == contract.CTypeCall || ctype == contract.CTypeTransfer) &&
-		to.Equal(state.SystemAddress) {
-		if h, ok := ch.(CallHandler); ok {
-			return newSystemHandler(h), nil
-		}
-	}
-	if h, ok := ch.(CallHandler); ok {
-		return newCallHandler(h, to, true), nil
-	}
-	return ch, nil
-}
-
-func (cm *contractManager) GetCallHandler(from, to module.Address, value *big.Int, ctype int, paramObj *codec.TypedObj) (contract.ContractHandler, error) {
-	switch ctype {
-	case contract.CTypeTransfer:
-		if !to.IsContract() {
-			return newTransferHandler(from, to, value, true, cm.log), nil
-		}
-	case contract.CTypeCall:
-		if !to.IsContract() {
-			return newTransferHandler(from, to, value, true, cm.log), nil
-		}
-	}
-	ch, err := cm.ContractManager.GetCallHandler(from, to, value, ctype, paramObj)
-	if err != nil {
-		return nil, err
-	}
-	if h, ok := ch.(CallHandler); ok {
-		return newCallHandler(h, to, false), nil
-	}
-	return ch, nil
-}
-
-const (
-	EETypesJavaOnly = string(state.JavaEE)
-)
 
 func newContractManager(_ *platform, dbase db.Database, dir string, logger log.Logger) (contract.ContractManager, error) {
 	cm, err := contract.NewContractManager(dbase, dir, logger)
 	if err != nil {
 		return nil, err
 	}
-	eeTypes, err := state.ParseEETypes(EETypesJavaOnly)
-	if err != nil {
-		return nil, errors.Wrapf(err, "InvalidEETypes(s=%s)", EETypesJavaOnly)
-	}
-	return &contractManager{cm, logger, eeTypes}, nil
+	return &contractManager{cm, logger}, nil
 }
