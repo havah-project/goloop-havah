31dee3041a5cb26d48e52852b7ffb9f06d8d8b15
KyoungNam Kim <kn.kim@icon.foundation>
2022-06-14 20:48:30 +0900

    add governance score test case
    - and add agent test for planetNFT

---
+++ testsuite/java/io/havah/test/cases/GovernanceTest.java
@@ -0,0 +1,150 @@
+package io.havah.test.cases;
+
+import foundation.icon.icx.IconService;
+import foundation.icon.icx.KeyWallet;
+import foundation.icon.icx.Wallet;
+import foundation.icon.icx.data.Bytes;
+import foundation.icon.icx.data.TransactionResult;
+import foundation.icon.icx.transport.http.HttpProvider;
+import foundation.icon.test.common.Env;
+import foundation.icon.test.common.TestBase;
+import foundation.icon.test.common.TransactionHandler;
+import io.havah.test.common.Constants;
+import io.havah.test.score.ChainScore;
+import io.havah.test.score.GovScore;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+
+import java.math.BigInteger;
+
+import static foundation.icon.test.common.Env.LOG;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@Tag(Constants.TAG_HAVAH)
+public class GovernanceTest extends TestBase {
+    /*
+    1. setUSDT & getUSDT
+    2. startRewardIssue
+        // startRewardIssue 이전 reportPlanetWork, claimPlanetReward는 의미없음.
+        // startRewardIssue 이후 reportPlanetWork, claimPlanetReward가 정상동작.
+    3. addPlanetManger
+    4. removePlanetManager
+    5. reportPlanetWork
+        4 - 1. deploy planetNft
+        4 - 2. mint planetNft to A
+        4 - 3. reportPlanetWork with B - failure
+        4 - 4. reportPlanetWork with A - success
+    6. claimPlanetReward()
+
+    &
+
+    test for sustainable fund
+    test for hoover fund
+     */
+    private static TransactionHandler txHandler;
+
+    private static ChainScore chainScore;
+    private static GovScore govScore;
+    private static KeyWallet[] testWallets;
+    private static final int testWalletNum = 3;
+    private static KeyWallet governorWallet;
+
+    @BeforeAll
+    public static void setup() {
+        Env.Node node = Env.nodes[0];
+        Env.Channel channel = node.channels[0];
+        Env.Chain chain = channel.chain;
+        IconService iconService = new IconService(new HttpProvider(channel.getAPIUrl(Env.testApiVer)));
+        txHandler = new TransactionHandler(iconService, chain);
+        govScore = new GovScore(txHandler);
+        chainScore = new ChainScore(txHandler);
+        governorWallet = txHandler.getChain().governorWallet;
+        try {
+            Bytes txHash = txHandler.transfer(chain.godWallet, governorWallet.getAddress(), ICX);
+            assertSuccess(txHandler.getResult(txHash));
+
+            testWallets = new KeyWallet[testWalletNum];
+            for (int i = 0; i < testWalletNum; i++) {
+                testWallets[i] = KeyWallet.create();
+            }
+
+            for (String type : new String[]{"invoke", "query"}) {
+//                assertSuccess(govScore.setMaxStepLimit(type, BigInteger.valueOf(10000000)));
+            }
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    void testAPIS() throws Exception {
+        LOG.infoEntering("setUSDTPrice", "setUSDTPrice : 200");
+        BigInteger price = chainScore.getUSDTPrice().add(BigInteger.ONE);
+        assertSuccess(govScore.setUSDTPrice(governorWallet, price));
+        LOG.infoExiting();
+
+        LOG.infoEntering("getUSDTPrice");
+        BigInteger changed = chainScore.getUSDTPrice();
+        assertEquals(price, changed);
+
+        LOG.infoEntering("addPlanetManager");
+        assertSuccess(govScore.addPlanetManager(governorWallet, testWallets[1].getAddress()));
+        LOG.infoExiting();
+
+//        LOG.infoEntering("reportPlanetWork");
+//        assertSuccess(govScore.reportPlanetWork(testWallets[1], BigInteger.ONE));
+//        LOG.infoExiting();
+
+        LOG.infoEntering("removePlanetManager");
+        assertSuccess(govScore.removePlanetManager(governorWallet, testWallets[1].getAddress()));
+        LOG.infoExiting();
+
+    }
+
+    void _checkUSDTPrice(Wallet wallet, boolean expected) throws Exception {
+        boolean success = wallet.equals(governorWallet);
+        LOG.infoEntering("checkUSDTPrice success(" + success + ")");
+        assertEquals(expected, success);
+        BigInteger price = chainScore.getUSDTPrice();
+        BigInteger change = price.add(BigInteger.ONE);
+        TransactionResult result = govScore.setUSDTPrice(wallet, change);
+        LOG.info("before(" + price + "), after(" + chainScore.getUSDTPrice());
+        LOG.info("Address(" + wallet.getAddress() + "), USDTPrice result(" + result + ")");
+        assertEquals(success ? 1 : 0, result.getStatus().intValue(), result.toString());
+        assertEquals(success ? change : price, chainScore.getUSDTPrice());
+        LOG.info("checkUSDTPrice");
+    }
+
+    @Test
+    void checkUSDTPrice() throws Exception {
+        Wallet tmp = testWallets[0];
+        _checkUSDTPrice(tmp, false);
+        _checkUSDTPrice(governorWallet, true);
+    }
+
+
+    void _checkPlanetManager(Wallet wallet, boolean expected) throws Exception {
+        boolean success = wallet.equals(governorWallet);
+        assertEquals(expected, success);
+        Wallet tmp = KeyWallet.create();
+
+        assertEquals(false, chainScore.isPlanetManager(tmp.getAddress()));
+        var txResult = govScore.addPlanetManager(wallet, tmp.getAddress());
+        assertEquals(success ? 1 : 0, txResult.getStatus().intValue());
+        assertEquals(success, chainScore.isPlanetManager(tmp.getAddress()));
+    }
+
+    @Test
+    void checkPlanetManager() throws Exception {
+        Wallet tmp = testWallets[0];
+        _checkPlanetManager(tmp, false);
+        _checkPlanetManager(governorWallet, true);
+    }
+
+//    @Test
+//    void managePlanetManager() {
+//
+//    }
+}
---
+++ testsuite/java/io/havah/test/score/GovScore.java
@@ -0,0 +1,248 @@
+package io.havah.test.score;
+
+import foundation.icon.icx.Wallet;
+import foundation.icon.icx.data.Address;
+import foundation.icon.icx.data.Bytes;
+import foundation.icon.icx.data.TransactionResult;
+import foundation.icon.icx.transport.jsonrpc.RpcItem;
+import foundation.icon.icx.transport.jsonrpc.RpcObject;
+import foundation.icon.icx.transport.jsonrpc.RpcValue;
+import foundation.icon.test.common.ResultTimeoutException;
+import foundation.icon.test.common.TransactionFailureException;
+import foundation.icon.test.common.TransactionHandler;
+import foundation.icon.test.score.Score;
+import io.havah.test.common.Constants;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class GovScore extends Score {
+//    private final Wallet governorWallet;
+    private final ChainScore chainScore;
+
+    public static class Fee {
+        Map<String, BigInteger> stepCosts;
+        Map<String, BigInteger> stepMaxLimits;
+        BigInteger stepPrice;
+    }
+
+    public static final String[] stepCostTypes = {
+            "default",
+            "contractCall",
+            "contractCreate",
+            "contractUpdate",
+            "contractDestruct",
+            "contractSet",
+            "get",
+            "set",
+            "replace",
+            "delete",
+            "input",
+            "eventLog",
+            "apiCall"
+    };
+
+    public GovScore(TransactionHandler txHandler) {
+        super(txHandler, Constants.GOV_ADDRESS);
+//        this.governorWallet = txHandler.getChain().governorWallet;
+        this.chainScore = new ChainScore(txHandler);
+    }
+
+//    private Wallet getWallet() {
+//        return this.governorWallet;
+//    }
+
+    @Override
+    public TransactionResult invokeAndWaitResult(Wallet wallet, String method, RpcObject params)
+            throws ResultTimeoutException, IOException {
+        return super.invokeAndWaitResult(wallet, method, params, BigInteger.ZERO, Constants.DEFAULT_STEPS);
+    }
+
+//    public TransactionResult setRevision(int code) throws Exception {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("code", new RpcValue(BigInteger.valueOf(code)))
+//                .build();
+//        return invokeAndWaitResult(getWallet(), "setRevision", params);
+//    }
+//
+//    public void setStepPrice(BigInteger price) throws Exception {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("price", new RpcValue(price))
+//                .build();
+//        invokeAndWaitResult(getWallet(), "setStepPrice", params);
+//    }
+//
+//    public void setStepCost(String type, BigInteger cost) throws ResultTimeoutException, IOException {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("type", new RpcValue(type))
+//                .put("cost", new RpcValue(cost))
+//                .build();
+//        invokeAndWaitResult(getWallet(), "setStepCost", params);
+//    }
+//
+//    public TransactionResult setMaxStepLimit(String type, BigInteger cost) throws ResultTimeoutException, IOException {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("contextType", new RpcValue(type))
+//                .put("limit", new RpcValue(cost))
+//                .build();
+//        return invokeAndWaitResult(getWallet(), "setMaxStepLimit", params);
+//    }
+//
+//    public boolean isAuditEnabledOnly() throws IOException {
+//        int config = chainScore.getServiceConfig();
+//        return ChainScore.isAuditEnabled(config) && !ChainScore.isDeployerWhiteListEnabled(config);
+//    }
+//
+//    public TransactionResult acceptScore(Bytes txHash) throws ResultTimeoutException, IOException {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("txHash", new RpcValue(txHash))
+//                .build();
+//        return invokeAndWaitResult(getWallet(), "acceptScore", params);
+//    }
+//
+//    public TransactionResult rejectScore(Bytes txHash) throws ResultTimeoutException, IOException {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("txHash", new RpcValue(txHash))
+//                .build();
+//        return invokeAndWaitResult(getWallet(), "rejectScore", params);
+//    }
+//
+//    public Map<String, BigInteger> getStepCosts() throws Exception {
+//        RpcItem rpcItem = this.chainScore.call("getStepCosts", null);
+//        Map<String, BigInteger> map = new HashMap<>();
+//        for(String type : stepCostTypes) {
+//            map.put(type, rpcItem.asObject().getItem(type).asInteger());
+//        }
+//        return map;
+//    }
+//
+//    public void setStepCosts(Map<String, BigInteger> map)
+//            throws ResultTimeoutException, TransactionFailureException, IOException {
+//        List<Bytes> list = new LinkedList<>();
+//        for(String type : map.keySet()) {
+//            RpcObject params = new RpcObject.Builder()
+//                    .put("type", new RpcValue(type))
+//                    .put("cost", new RpcValue(map.get(type)))
+//                    .build();
+//            Bytes txHash = invoke(getWallet(), "setStepCost", params);
+//            list.add(txHash);
+//        }
+//        for(Bytes txHash : list) {
+//            TransactionResult result = getResult(txHash);
+//            if (!Constants.STATUS_SUCCESS.equals(result.getStatus())) {
+//                throw new TransactionFailureException(result.getFailure());
+//            }
+//        }
+//    }
+//
+//    public Map<String, BigInteger> getMaxStepLimits() throws Exception {
+//        Map<String, BigInteger> map = new HashMap<>();
+//        String[] types = {"invoke", "query"};
+//        for(String t : types) {
+//            RpcObject params = new RpcObject.Builder()
+//                    .put("contextType", new RpcValue(t))
+//                    .build();
+//            BigInteger stepLimit = this.chainScore.call("getMaxStepLimit", params).asInteger();
+//            map.put(t, stepLimit);
+//        }
+//        return map;
+//    }
+//
+//    public void setMaxStepLimits(Map<String, BigInteger> limits)
+//            throws ResultTimeoutException, TransactionFailureException, IOException {
+//        List<Bytes> list = new LinkedList<>();
+//        for(String type : limits.keySet()) {
+//            RpcObject params = new RpcObject.Builder()
+//                    .put("contextType", new RpcValue(type))
+//                    .put("limit", new RpcValue(limits.get(type)))
+//                    .build();
+//            Bytes txHash = invoke(getWallet(), "setMaxStepLimit", params);
+//            list.add(txHash);
+//        }
+//        for(Bytes txHash : list) {
+//            TransactionResult result = getResult(txHash);
+//            if (!Constants.STATUS_SUCCESS.equals(result.getStatus())) {
+//                throw new TransactionFailureException(result.getFailure());
+//            }
+//        }
+//    }
+//
+//    public Fee getFee() throws Exception {
+//        Fee fee = new Fee();
+//        fee.stepCosts = getStepCosts();
+//        fee.stepMaxLimits = getMaxStepLimits();
+//        fee.stepPrice = this.chainScore.getStepPrice();
+//        return fee;
+//    }
+//
+//    public void setFee(Fee fee) throws Exception {
+//        setStepPrice(fee.stepPrice);
+//        setStepCosts(fee.stepCosts);
+//        setMaxStepLimits(fee.stepMaxLimits);
+//    }
+//
+//    public TransactionResult addDeployer(Address address) throws IOException, ResultTimeoutException {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("address", new RpcValue(address))
+//                .build();
+//        return invokeAndWaitResult(getWallet(), "addDeployer", params);
+//    }
+//
+//    public TransactionResult removeDeployer(Address address) throws IOException, ResultTimeoutException {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("address", new RpcValue(address))
+//                .build();
+//        return invokeAndWaitResult(getWallet(), "removeDeployer", params);
+//    }
+//
+//    public TransactionResult setDeployerWhiteListEnabled(boolean yn) throws IOException, ResultTimeoutException {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("yn", new RpcValue(yn))
+//                .build();
+//        return invokeAndWaitResult(getWallet(), "setDeployerWhiteListEnabled", params);
+//    }
+
+    // APIs for havah
+    public TransactionResult setUSDTPrice(Wallet wallet, BigInteger price) throws IOException, ResultTimeoutException {
+        RpcObject params = new RpcObject.Builder()
+                .put("price", new RpcValue(price))
+                .build();
+        return invokeAndWaitResult(wallet, "setUSDTPrice", params);
+    }
+
+//    public int getUSDTPrice() throws IOException {
+//        return call("getUSDTPrice", null).asInteger().intValue();
+//    }
+
+    public TransactionResult addPlanetManager(Wallet wallet, Address address) throws IOException, ResultTimeoutException {
+        RpcObject params = new RpcObject.Builder()
+                .put("address", new RpcValue(address))
+                .build();
+        return invokeAndWaitResult(wallet, "addPlanetManager", params);
+    }
+
+    public TransactionResult removePlanetManager(Wallet wallet, Address address) throws IOException, ResultTimeoutException {
+        RpcObject params = new RpcObject.Builder()
+                .put("address", new RpcValue(address))
+                .build();
+        return invokeAndWaitResult(wallet, "removePlanetManager", params);
+    }
+
+//    public TransactionResult claimPlanetReward(BigInteger[] ids) throws IOException, ResultTimeoutException {
+//        RpcObject params = new RpcObject.Builder()
+//                .put("ids", new RpcValue(ids))
+//                .build();
+//        return invokeAndWaitResult(getWallet(), "claimPlanetReward", params);
+//    }
+
+    public TransactionResult startRewardIssue(Wallet wallet, BigInteger height) throws IOException, ResultTimeoutException {
+        RpcObject params = new RpcObject.Builder()
+                .put("height", new RpcValue(height))
+                .build();
+        return invokeAndWaitResult(wallet, "startRewardIssue", params);
+    }
+}
