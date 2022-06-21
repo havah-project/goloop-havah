c7caa24bb6779c8adeffafdc449157562c51cac1
Chiwon Cho <superdoli@gmail.com>
2022-06-21 18:47:14 +0900

    Add unit-test for State.ClaimPlanetReward()
    * Need to add the case of claiming for a private planet

--- havah/hvh/hvhstate/state_test.go
+++ havah/hvh/hvhstate/state_test.go
@@ -16,11 +16,19 @@ func newDummyState() *State {
 	return NewStateFromSnapshot(snapshot, false, nil)
 }
 
-func newDummyPlanet(height int64) *Planet {
+func newDummyPlanet(isPrivate, isCompany bool, height int64) *Planet {
 	owner := common.MustNewAddressFromString("hx123")
 	usdt := big.NewInt(1000)
 	price := big.NewInt(2000)
-	return newPlanet(false, false, owner, usdt, price, height)
+	return newPlanet(isPrivate, isCompany, owner, usdt, price, height)
+}
+
+func newPlanetReward(total, current *big.Int, lastTN int64) *planetReward {
+	return &planetReward{
+		total:   total,
+		lastTN:  lastTN,
+		current: current,
+	}
 }
 
 func checkAllPlanet(t *testing.T, state *State, expected int64) {
@@ -68,6 +76,16 @@ func deepCopyPlanetReward(t *testing.T, pr *planetReward) *planetReward {
 		t.Errorf("deepCopyPlanetReward() error")
 	}
 	return pr2
+}
+
+func toUSDT(value int64) *big.Int {
+	usdt := big.NewInt(value)
+	return usdt.Mul(usdt, hvhmodule.BigIntUSDTDecimal)
+}
+
+func toHVH(value int64) *big.Int {
+	hvh := big.NewInt(value)
+	return hvh.Mul(hvh, hvhmodule.BigIntUSDTDecimal)
 }
 
 func TestState_SetUSDTPrice(t *testing.T) {
@@ -306,7 +324,7 @@ func TestState_GetPlanet(t *testing.T) {
 
 	dictDB := state.getDictDB(hvhmodule.DictPlanet, 1)
 
-	p = newDummyPlanet(1234)
+	p = newDummyPlanet(false, false, 1234)
 	if err = state.setPlanet(dictDB, id, p); err != nil {
 		t.Errorf(err.Error())
 	}
@@ -436,13 +454,13 @@ func TestState_DecreaseRewardRemain(t *testing.T) {
 func TestState_calcClaimableReward(t *testing.T) {
 	height := int64(100)
 	owner := common.MustNewAddressFromString("hx12345")
-	usdt := new(big.Int).Mul(big.NewInt(5000), hvhmodule.BigIntUSDTDecimal)   // $5000
-	price := new(big.Int).Mul(big.NewInt(50000), hvhmodule.BigIntCoinDecimal) // 50000 HVH
+	usdt := toUSDT(5000)
+	price := toHVH(50000)
 	p := newPlanet(false, false, owner, usdt, price, 100)
 	pr := newEmpyPlanetReward()
 	state := newDummyState()
 
-	amount := big.NewInt(1000)
+	amount := toHVH(1)
 	if err := pr.increment(1, new(big.Int).Set(amount)); err != nil {
 		t.Errorf("planetReward.increment() error")
 	}
@@ -465,21 +483,78 @@ func TestState_calcClaimableReward(t *testing.T) {
 	}
 }
 
-/*
 func TestState_ClaimPlanetReward(t *testing.T) {
-	const id = 1
-	const height = 100
-	planet := newDummyPlanet(height)
+	const (
+		publicId  = 1
+		companyId = 2
+		privateId = 3
+	)
+	issueStart := int64(100)
+	owner := common.MustNewAddressFromString("hx1234")
+
 	state := newDummyState()
 
+	ps := []*Planet{
+		nil,
+		newPlanet(false, false, owner, toUSDT(5000), toHVH(50000), 10), // public
+		newPlanet(false, true, owner, toUSDT(2000), toHVH(20000), 10),  // company
+		newPlanet(true, false, owner, toUSDT(3000), toHVH(30000), 10),  // private
+	}
+	prs := []*planetReward{
+		nil,
+		newPlanetReward(toHVH(10), toHVH(3), 0),
+		newPlanetReward(toHVH(10), toHVH(1), 0),
+		newPlanetReward(toHVH(10), toHVH(2), 0),
+	}
+	for i := 1; i < 4; i++ {
+		pr := deepCopyPlanetReward(t, prs[i])
+		if err := state.setPlanetReward(int64(i), pr); err != nil {
+			t.Errorf(err.Error())
+		}
+	}
+
 	planetDictDB := state.getDictDB(hvhmodule.DictPlanet, 1)
-	if err := state.setPlanet(planetDictDB, id, deepCopyPlanet(t, planet)); err != nil {
+	for i := 1; i < 4; i++ {
+		p := deepCopyPlanet(t, ps[i])
+		if err := state.setPlanet(planetDictDB, int64(i), p); err != nil {
+			t.Errorf(err.Error())
+		}
+	}
+
+	if err := state.SetIssueStart(1, issueStart); err != nil {
 		t.Errorf(err.Error())
 	}
 
-	reward, err := state.ClaimPlanetReward(id, 200, planet.Owner())
-	if reward == nil && err != nil {
+	height := state.GetIssueStart() + state.GetTermPeriod()
+	for _, id := range []int64{publicId, companyId} {
+		reward, err := state.ClaimPlanetReward(id, height, owner)
+		if reward == nil && err != nil {
+			t.Errorf("ClaimPlanetReward() error")
+		}
+		if reward.Cmp(prs[id].Current()) != 0 {
+			t.Errorf("ClaimPlanetReward() error")
+		}
+		pr, err := state.GetPlanetReward(id)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		if pr.Total().Cmp(prs[id].Total()) != 0 {
+			t.Errorf("Incorrect total")
+		}
+		if pr.Current().Sign() != 0 {
+			t.Errorf("Incorrect current")
+		}
+		if pr.LastTermNumber() != prs[id].LastTermNumber() {
+			t.Errorf("Incorrect lastTN")
+		}
+	}
 
+	id := int64(privateId)
+	reward, err := state.ClaimPlanetReward(id, height, owner)
+	if reward == nil && err != nil {
+		t.Errorf("ClaimPlanetReward() error")
+	}
+	if reward.Sign() != 0 {
+		t.Errorf("ClaimPlanetReward() error")
 	}
 }
-*/
