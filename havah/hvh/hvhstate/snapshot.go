34544492dc10595341318b06db48f0e0890a5a06
MoonKyu Song <mksong@iconloop.com>
2022-07-01 10:25:15 +0900

    Remove unused state.Snapshot.NewState()

--- havah/hvh/hvhstate/snapshot.go
+++ havah/hvh/hvhstate/snapshot.go
@@ -5,7 +5,6 @@ import (
 	"github.com/icon-project/goloop/common/merkle"
 	"github.com/icon-project/goloop/common/trie"
 	"github.com/icon-project/goloop/common/trie/trie_manager"
-	"github.com/icon-project/goloop/havah/hvhutils"
 )
 
 type Snapshot struct {
@@ -25,11 +24,6 @@ func (ss *Snapshot) Flush() error {
 
 func (ss *Snapshot) GetValue(key []byte) ([]byte, error) {
 	return ss.store.Get(key)
-}
-
-func (ss *Snapshot) NewState(readonly bool) *State {
-	logger := hvhutils.NewLogger(nil)
-	return NewStateFromSnapshot(ss, readonly, logger)
 }
 
 func NewSnapshot(dbase db.Database, h []byte) *Snapshot {
