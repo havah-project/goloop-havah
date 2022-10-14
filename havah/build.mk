e6e38c4b2695d634a4600f989028ce1e64111638
Chiwon Cho <superdoli@gmail.com>
2022-10-14 19:45:27 +0900

    BugFix docker image bulid error in havah/build.mk

--- havah/build.mk
+++ havah/build.mk
@@ -3,7 +3,7 @@
 #
 
 GOCHAIN_HAVAH_IMAGE = goloop/gochain-havah:$(GL_TAG)
-GOCHAIN_HAVAH_IMAGE = $(BUILD_DIR)/gochain-havah
+GOCHAIN_HAVAH_DOCKER_DIR = $(BUILD_DIR)/gochain-havah
 
 GOLOOP_HAVAH_IMAGE = goloop-havah:$(GL_TAG)
 GOLOOP_HAVAH_DOCKER_DIR = $(BUILD_DIR)/goloop-havah
@@ -16,7 +16,7 @@ gochain-havah-image: base-image-java gorun-gochain javarun-javaexec
 	IMAGE_BASE=$(BASE_JAVA_IMAGE) \
 	GOCHAIN_HAVAH_VERSION=$(GL_VERSION) \
 	GOBUILD_TAGS="$(GOBUILD_TAGS)" \
-	$(BUILD_ROOT)/docker/gochain-havah/update.sh $(GOCHAIN_HAVAH_IMAGE) $(BUILD_ROOT) $(GOCHAIN_HAVAH_IMAGE)
+	$(BUILD_ROOT)/docker/gochain-havah/update.sh $(GOCHAIN_HAVAH_IMAGE) $(BUILD_ROOT) $(GOCHAIN_HAVAH_DOCKER_DIR)
 
 goloop-havah-image: base-image-java gorun-goloop javarun-javaexec
 	@ echo "[#] Building image $(GOLOOP_HAVAH_IMAGE) for $(GL_VERSION)"
